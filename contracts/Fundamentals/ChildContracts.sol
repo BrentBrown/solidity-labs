c83691f6668a302bb8ed8d6f633bac0f0e3f5b4b
Brent Brown <aws-code-commit.t1s3o@simplelogin.com>
2025-05-13 13:57:43 -0600

    Added more examples

---
+++ contracts/Fundamentals/ChildContracts.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.15;
+
+//In general child contracts will cost more gas than structs.
+//They are also more complex to manage.
+
+contract PaymentReceived {
+    address public from;
+    uint public amount;
+
+    constructor(address _from, uint _amount) {
+        from = _from;
+        amount = _amount;
+    }
+}
+
+contract Wallet {
+    PaymentReceived public payment;
+
+    function payContract() public payable {
+        payment = new PaymentReceived(msg.sender, msg.value);
+    }
+}
